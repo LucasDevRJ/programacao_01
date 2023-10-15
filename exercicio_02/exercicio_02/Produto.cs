1b1ad69fb6d730d3fe973d228b19b2961c4ac339
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-15 17:17:54 -0300

    algoritmo que adiciona e remove quantidade de produtos

---
+++ exercicio_02/exercicio_02/Produto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace exercicio_02
+{
+    internal class Produto
+    {
+        //declarando atributos
+        private string nome;
+        private double preco;
+        private int quantidade;
+
+        //encapuslando atributos
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setPreco(double preco)
+        {
+            this.preco = preco;
+        }
+       
+        public double valorTotalEmEstoque()
+        {
+            double valorTotal = this.preco * this.quantidade;
+            return valorTotal;
+        }
+
+        //método para adicionar produto
+        public void adicionarProduto(int quantidadeDesejada)
+        {
+            //condição para verificar se o valor é menor ou igual a 0
+            if (quantidadeDesejada <= 0)
+            {
+                Console.WriteLine("Digite um valor maior que 0!!\n");
+                quantidadeDesejada = Int32.Parse(Console.ReadLine());
+            }
+            else
+            {
+                this.quantidade += quantidadeDesejada;
+                Console.Write("Foram adicionadas " + quantidadeDesejada + " unidades do " + this.nome);
+            }
+            exibeInformacoesProduto();
+        }
+
+        //método para remover produto
+        public void removerProduto(int quantidadeDesejada)
+        {
+            //condição para verificar se o valor é menor ou igual a 0 e a qauntidade desejada é maior que a do produto
+            if (quantidadeDesejada <= 0 && this.quantidade < quantidadeDesejada)
+            {
+                Console.WriteLine("Adicione um valor maior que 0 e/ou quantidade estocada insuficiente!!\n");
+                quantidadeDesejada = Int32.Parse(Console.ReadLine());
+            }
+            else
+            {
+                this.quantidade -= quantidadeDesejada;
+                Console.Write("Foram removidas " + quantidadeDesejada + " unidades do " + this.nome);
+            }
+            exibeInformacoesProduto();
+        }
+
+        //método para exibir informações do produto
+        public void exibeInformacoesProduto()
+        {
+            Console.WriteLine("\nProduto: " + this.nome);
+            Console.WriteLine("Preço: " + this.preco);
+            Console.WriteLine("Quantidade: " + this.quantidade);
+            Console.WriteLine("Valor Total: R$ " + valorTotalEmEstoque());
+        }
+    }
+}
---
+++ exercicio_02/exercicio_02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio_02
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //instanciândo o Objeto Produto e chamando os métodos dele
+            Produto produto = new Produto();
+            produto.setNome("TV");
+            produto.setPreco(900.00);
+            produto.adicionarProduto(10);
+            produto.adicionarProduto(3);
+
+            Console.WriteLine("\n");
+            Produto produto2 = new Produto();
+            produto2.setNome("TV");
+            produto2.setPreco(900.00);
+            produto2.adicionarProduto(15);
+            produto2.removerProduto(3);
+        }
+    }
+}
