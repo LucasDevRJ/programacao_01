52da2cda2ef2cd98d2f1c56f20ffba2f146ced18
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-30 19:53:47 -0300

    mais tipos de variáveis

--- aula_1/aula_1/Program.cs
+++ aula_1/aula_1/Program.cs
@@ -4,6 +4,7 @@ namespace Aula1
 {
     class Program
     {
+        char c = 'a';
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World - Aula Prática 1!");
@@ -12,6 +13,13 @@ namespace Aula1
             int inteiro = 10;
 
             Console.WriteLine($"{inteiro}");
+
+            bool boleano1 = true;
+            bool boleano2 = false;
+
+            long longo = 33444555443;
+            decimal deci = 12.45M;
+            float f1 = 3445.23f;
         }
     }
 }
