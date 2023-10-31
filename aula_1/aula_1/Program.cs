27d6a6b64033c2bdeeb257390762188535e06f8f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-30 21:02:27 -0300

    exemplo de vetor

--- aula_1/aula_1/Program.cs
+++ aula_1/aula_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Aula1
 {
@@ -17,9 +18,38 @@ namespace Aula1
             bool boleano1 = true;
             bool boleano2 = false;
 
+            Console.WriteLine($"{boleano1}");
+            Console.WriteLine($"{boleano2}");
+
             long longo = 33444555443;
             decimal deci = 12.45M;
             float f1 = 3445.23f;
+
+            Console.WriteLine($"{longo}");
+            Console.WriteLine($"{deci}");
+            Console.WriteLine($"{f1}");
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Tipos references types");
+
+            object objeto1 = new object();
+            var objeto2 = new object();
+
+            var objeto3 = objeto2;
+
+            Console.WriteLine("Comparando objetos");
+            Console.WriteLine($"{objeto1.GetType().Name} | {objeto2.GetType().Name} " +
+                $"| {objeto3.GetType().Name}");
+            Console.WriteLine($"{objeto2 == objeto3}");
+
+            Console.WriteLine("-----------------------------");
+
+            var s1 = new string('a', 5);
+            string s2 = new string(new char[5] { 'a' , 'a', 'a', 'a', 'a'});
+            Console.WriteLine($"{s1 == s2}");
+
+            string s3 = string.Concat((new char[5] { 'a', 'a', 'a', 'a', 'a' }).AsEnumerable());
+            Console.WriteLine($"{s3}");
         }
     }
 }
