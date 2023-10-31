e57105bd13671b4c589857efbaa8e9bdcb5b31e9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-31 18:20:19 -0300

    criação de enumerador

--- aula_1/aula_1/ClasseComplexa.cs
+++ aula_1/aula_1/ClasseComplexa.cs
@@ -11,5 +11,18 @@ namespace aula_1
         public int PropInt { get; set; }
         public bool PropBool { get; set; }
         public decimal MeuDecimal { get; set; }
+
+        DateTime dataAtual;
+        float floatField;
+
+        public Direcoes Direcao { get; set; }
+
+        public enum Direcoes
+        {
+            Cima = 1,
+            Baixo = 10,
+            LadoEsquerdo = 20,
+            LadoDireito = 67
+        }
     }
 }
