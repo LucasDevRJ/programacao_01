9454d581c48a239011d823f3fbb1c03bdb869ee8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-31 18:16:41 -0300

    criação da classe complexa com seus atributos

---
+++ aula_1/aula_1/ClasseComplexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_1
+{
+    internal class ClasseComplexa
+    {
+        public int PropInt { get; set; }
+        public bool PropBool { get; set; }
+        public decimal MeuDecimal { get; set; }
+    }
+}
