d664bd07cf896de763efa8f7f54fcd44ee0198f1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-08 20:38:35 -0300

    Exercício 1 concluído

---
+++ aula_2/aula_2/Exercicio1.cs
@@ -0,0 +1,44 @@
+﻿namespace aula_2
+{
+    internal class Exercicio1
+    {
+        public void gerarNumerosInteiros()
+        {
+            int[] vetorComNumerosInteiros = new int[100000];
+            int[,] matrizComNumerosInteiros = new int[100, 100];
+
+            var numeroRandomico = new Random();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                vetorComNumerosInteiros[i] = i;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int k = 0; k < 100; k++)
+                {
+                    matrizComNumerosInteiros[i, k] = numeroRandomico.Next(1, 300000);
+                }
+            }
+
+            int contador = 0;
+            for (int i = 0; i < matrizComNumerosInteiros.GetUpperBound(0); i++)
+            {
+                for (int k = 0; k < matrizComNumerosInteiros.GetUpperBound(1); k++)
+                {
+                    var valorMatriz = (int) matrizComNumerosInteiros.GetValue(1, k);
+
+                    for (int x = 0; x < vetorComNumerosInteiros.Length; x++)
+                    {
+                        if (vetorComNumerosInteiros[x] == valorMatriz)
+                        {
+                            contador++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Possui {contador} números rândomicos");
+        }
+    }
+}
---
+++ aula_2/aula_2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercicio1 exercicio = new Exercicio1();
+            exercicio.gerarNumerosInteiros();
+        }
+    }
+}
