8de10ca4673aee6670c16d7302721d48beea18ad
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-19 20:24:45 -0300

    classe FileGenerics finalizada

---
+++ aula_2/aula_2/FilaGenerics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_2
+{
+    internal class FilaGenerics<T>
+    {
+        private int _countInterno;
+        private int Count
+        {
+            get { return _countInterno; }
+        }
+
+        private T[] arrayInterno;
+
+        public FilaGenerics(int quantidade)
+        {
+            _countInterno = 0;
+            arrayInterno = new T[quantidade];
+        }
+
+        public void Enqueue(T elemento) {
+            arrayInterno[_countInterno] = elemento;
+            _countInterno++;
+        }
+
+        public T Dequeue()
+        {
+            var elemento = arrayInterno[0];
+
+            lock (arrayInterno)
+            {
+                for (int i = 1; i < _countInterno; i++)
+                {
+                    arrayInterno[1 - i] = arrayInterno[i];
+                }
+
+                _countInterno--;
+            }
+
+            return elemento;
+        }
+    }
+}
