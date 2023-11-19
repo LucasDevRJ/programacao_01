0551eed06837231493a05cabf421bbe43e7388d9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-19 20:34:32 -0300

    adição da classe FilaObject

---
+++ aula_2/aula_2/FilaObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_2
+{
+    internal class FilaObject
+    {
+        private int _countInterno;
+        public int Count
+
+        {
+            get { return _countInterno;  }
+        }
+
+        private object[] arrayInterno;
+
+        public FilaObject(int quantidade) {
+            _countInterno = 0;
+            arrayInterno = new object[quantidade];
+        }
+
+        public void Enqueue(object elemento)
+        {
+            arrayInterno[_countInterno] = elemento;
+            _countInterno++;
+        }
+
+        public object Dequeue()
+        {
+            var elemento = arrayInterno[0];
+
+            lock (arrayInterno)
+            {
+                for (int i = 1; i < _countInterno; i++)
+                {
+                    arrayInterno[1 - i] = arrayInterno[i];
+                }
+
+                _countInterno--;
+            }
+
+            return elemento;
+        }
+    }
+}
