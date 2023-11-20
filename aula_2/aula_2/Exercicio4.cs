a2e46f15584b3082874eb93da5763962a1186fd3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-20 08:12:22 -0300

    Criação da classe Gato

--- aula_2/aula_2/Exercicio4.cs
+++ aula_2/aula_2/Exercicio4.cs
@@ -8,20 +8,23 @@ namespace aula_2
 {
     internal class Exercicio4
     {
-        //iniciando listas
-        var filaGenerics = new FilaGenerics<Animal>(10);
-        var filaObject = new FilaObject(10);
+        public void ExercicioBoxingFilas()
+        {
+            //iniciando listas
+            var filaGenerics = new FilaGenerics<Animal>(10);
+            var filaObject = new FilaObject(10);
 
-        //instância das classes filhas
-        Cachorro cachorro = new Cachorro();
-        Gato gato = new Gato();
+            //instância das classes filhas
+            Cachorro cachorro = new Cachorro();
+            Gato gato = new Gato();
 
-        //instância com box para tipo "pai"
-        Animal cachorro2 = new Cachorro();
-        Animal gato2 = new Gato();
+            //instância com box para tipo "pai"
+            Animal cachorro2 = new Cachorro();
+            Animal gato2 = new Gato();
 
-        //instância com box para interface
-        IBarulhento cachorro3 = new Cachorro();
-        IBarulhento gato3 = new Gato();
+            //instância com box para interface
+            IBarulhento cachorro3 = new Cachorro();
+            IBarulhento gato3 = new Gato();
+        }
     }
 }
