bbd8790bcf4ac61214c48509341326f25966d86d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-20 08:00:57 -0300

    Criação da classe abstrata Animal

---
+++ aula_2/aula_2/Exercicio4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_2
+{
+    internal class Exercicio4
+    {
+        //iniciando listas
+        var filaGenerics = new FilaGenerics<Animal>(10);
+        var filaObject = new FilaObject(10);
+
+        //instância das classes filhas
+        Cachorro cachorro = new Cachorro();
+        Gato gato = new Gato();
+
+        //instância com box para tipo "pai"
+        Animal cachorro2 = new Cachorro();
+        Animal gato2 = new Gato();
+
+        //instância com box para interface
+        IBarulhento cachorro3 = new Cachorro();
+        IBarulhento gato3 = new Gato();
+    }
+}
