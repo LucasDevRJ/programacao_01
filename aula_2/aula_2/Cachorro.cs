60e694c0ea977f98b0635f2b7e453c8fa8785c51
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-20 08:09:39 -0300

    classe Cachorro

---
+++ aula_2/aula_2/Cachorro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_2
+{
+    internal class Cachorro : Animal
+    {
+        public override string EmitirSom()
+        {
+            return "Au Au!!";
+        }
+    }
+}
