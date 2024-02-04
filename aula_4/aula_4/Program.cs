8dcf40de00a34ea11402e00880c1536513916f4c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-02-04 11:11:59 -0300

    Exemplo de thread

--- aula_4/aula_4/Program.cs
+++ aula_4/aula_4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace aula_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula Prática 4!");
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Threads");
+            Console.WriteLine("--------------------");
+
+            ThreadPing exercicio1 = new ThreadPing();
+            exercicio1.StartPing();
+        }
+    }
+}
--- aula_4/aula_4/ThreadPing.cs
+++ aula_4/aula_4/ThreadPing.cs
@@ -1,14 +1,55 @@
-﻿namespace aula_4
+﻿using System.Net.NetworkInformation;
+
+namespace aula_4
 {
-    internal class ThreadPing
+    public class ThreadPing()
     {
-        public ThreadPing()
-        {
-        }
+        private string endereco = "google.com";
+        int countPing = 0;
+        bool executePing = false;
 
         internal void StartPing()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Digite S a qualquer momento para interromper");
+
+            var threadPingger = new Thread(ExecutePing);
+            threadPingger.Start();
+            executePing = true;
+
+            var comandoSair = "S";
+            var comando = string.Empty;
+
+            //while (!comandoSair.Equals(comando))
+            //{
+            //    comando = Console.ReadLine();
+            //}
+
+            executePing = false;
+
+            while (threadPingger.IsAlive)
+            {
+                Console.WriteLine("Esperando finalizar...");
+
+                threadPingger.Join();
+            }
+
+            Console.WriteLine("Thread finalizada.");
+        }
+
+        public void ExecutePing()
+        {
+            while (countPing < 4)
+            {
+                Ping pingger = new Ping();
+
+                var pingResponse = pingger.Send(endereco);
+
+                Console.WriteLine($"Ping {countPing}: {endereco} | Status: {pingResponse.Status} - {pingResponse.RoundtripTime}ms");
+                countPing++;
+
+                //espera alguns segundos
+                Thread.Sleep(1000);
+            }
         }
     }
 }
